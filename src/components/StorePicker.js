0f7e0711702d7c09cbc784d42b3ffa09d72df326
iamkartik <kartiksharma1891@gmail.com>
2017-09-16 20:43:14 +0530

    added jsx for storePicker and added styles

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -6,7 +6,19 @@ class StorePicker extends React.Component{
     // essential to have a render method
     render(){
         // return jsx
-        return <p>Hello World !!</p>
+        // return html inside () 
+        // can only return one parent,multiple elements at same parent level will cause error
+        return (
+            // cannot use class for css, as it is a keyword in js
+            // use className instead
+            <form className="store-selector">
+                <h2>Please Enter a Store</h2>
+                 {/*cannot put comment over parent tag*/}
+                 {/*all tags must be closed*/} 
+                <input type="text" placeholder="Store Name" required/>
+                <button type="submit">Visit Store</button>
+            </form>
+        );
     }
 };
 // exporting the entire class 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 // react can render to native or dom ,get dom to render on html page 
 // only importing the render mehod from ReactDom
 import { render } from 'react-dom';
+// inport css to allow webpack to compile all
+import './css/style.css';
 import StorePicker from './components/StorePicker';
 
 // tell the  render method to render which component and where
