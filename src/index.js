33c91e3b84a5f4fb3610bea76097578c71ec6692
iamkartik <kartiksharma1891@gmail.com>
2017-09-17 17:00:24 +0530

    created Root component, added React-Routing to enable which components render/not,created a NotFound component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,33 @@ import React from 'react';
 // react can render to native or dom ,get dom to render on html page 
 // only importing the render mehod from ReactDom
 import { render } from 'react-dom';
-// inport css to allow webpack to compile all
+// impoting functions from React Router(used for routing in a react app)
+import { BrowserRouter, Match, Miss } from 'react-router';
+// import css to allow webpack to compile all
 import './css/style.css';
 import StorePicker from './components/StorePicker';
 import App from './components/App';
+import NotFound from './components/NotFound';
+
+
+// The root component , stateless functional
+// according to the url it will show/hide components
+const Root =()=>{
+    return (
+        <BrowserRouter>
+            {/*wrapping matches inside a div as  React.Children.only expected to receive a single React element child.*/}
+            <div>
+                {/*if a route is default show the store picker component*/}
+                <Match exactly pattern="/" component={StorePicker}/>
+                {/*if a route pattern matches /store/... show the main App component*/}
+                <Match pattern="/store/:storeId" component={App}/>
+                {/*if all the matches fail then go to miss*/}
+                <Miss component={NotFound}/>
+            </div>
+        </BrowserRouter>    
+    );
+};
+
 
 // tell the  render method to render which component and where
-// StorePicker component is rendered on main div inside index.html
-render(<App/>,document.querySelector('#main'));
+render(<Root/>,document.querySelector('#main'));
