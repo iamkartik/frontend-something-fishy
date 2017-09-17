d8809374a7a2853dac4a633d838d5265d2daffe8
iamkartik <kartiksharma1891@gmail.com>
2017-09-17 18:01:23 +0530

    created the onSubmit event to redirect to store. Using Ref to access the DOM element, using different binding techniques to bind goToStore to StorePicker class

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,21 +1,48 @@
 // each component should have a seperate file 
 import React from 'react';
+// imnporting fun names from helper
+import { getFunName } from '../helpers'
 
 // React component created by extendind React.Component
 class StorePicker extends React.Component{
+/*
+    constructor(){
+        super();
+        // binding the go to store to the class
+        // the method equals , the method bound to this(class)
+        this.goToStore = this.goToStore.bind(this);
+    }*/
+
+    goToStore(e){
+        e.preventDefault();
+        // grab the text from text from input
+        // instead of using the querySelectors to modify DOM , React uses ref to access elements
+        // inside render this refers to the class , but inside other methods it is not by default
+        // other methods are not implicitly bind to the class
+        // to access this , bind the method inside the constructor or bind this to the event trigger inside render
+        console.log(this.storeInput.value)
+    }// es6 class no comma required
+
+
     // essential to have a render method
     render(){
         // return jsx
         // return html inside () 
         // can only return one parent,multiple elements at same parent level will cause error
+        // react events put inline , not using selectores and adding events
         return (
             // cannot use class for css, as it is a keyword in js
             // use className instead
-            <form className="store-selector">
+            // render method bound to class it is created ,thus this always refer to the class
+            // binding  the goToStore  to class ,or use (e)=>this.goToStore(e)  
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
                 <h2>Please Enter a Store</h2>
                  {/*cannot put comment over parent tag*/}
-                 {/*all tags must be closed*/} 
-                <input type="text" placeholder="Store Name" required/>
+                 {/*all tags must be closed*/}
+                 {/*value attribute tied to state thus using defaultValue*/} 
+                 {/*ref is a callback function ,once the element is rendered , it keeps a reference to it in react object ($r)*/} 
+                <input type="text" placeholder="Store Name" required defaultValue={getFunName()} 
+                    ref={(input)=>this.storeInput=input}/>
                 <button type="submit">Visit Store</button>
             </form>
         );
